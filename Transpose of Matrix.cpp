09fdd7f3963ab8e462564dd2b8cdfae57c4106a2
Mohit Saini <61094186+mskSaini01@users.noreply.github.com>
2023-07-10 21:43:37 +0530

    potd (10-7-2023)

---
+++ Transpose of Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
